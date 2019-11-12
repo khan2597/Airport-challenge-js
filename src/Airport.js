d0427203d404203c789b612a5e45bd80fec4097d
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-11-12 08:48:38 +0000

    On storm part of airport challenge

---
+++ src/Airport.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function Airport() {
+    this._hanger = [];
+}
+
+Airport.prototype.planes = function(){ 
+    return this._hanger; 
+};
+
+Airport.prototype.clearForLanding = function(plane) {
+    if(this.isStormy()) {
+        throw new Error('cannot land during storm')
+    }
+    this._hanger.push(plane);
+};
+
+Airport.prototype.clearForTakeOff = function(plane) {
+    if(this.isStormy()) {
+        throw new Error('cannot takeoff during storm');
+    }
+    this._hanger = [];
+};
+
+Airport.prototype.isStormy = function() {
+    return false;
+};
